ab70a71165750ca3ee9f58e1e2738f298f0d3112
umgihtgkr <105083015+umgihtgkr@users.noreply.github.com>
2022-06-05 08:44:19 +0200

    Update start.py

--- program/start.py
+++ program/start.py
@@ -197,20 +197,20 @@ def echo(client, msg):
 def forward(client: Client, message: Message):
   chat_id = message.chat.id
   user_id = message.from_user.id
-  rank = app.get_chat_member(chat_id, user_id)
+  rank = Client.get_chat_member(chat_id, user_id)
   rank = rank.status
   if rank == "administrator":
-   app.send_message(chat_id,"مسؤال")
+   Client.send_message(chat_id,"مسؤال")
   elif rank == "creator":
-   app.send_message(chat_id,"المنشئ يعم")
+   Client.send_message(chat_id,"المنشئ يعم")
   elif rank == "member":
-   app.send_message(chat_id,"عضو زليل")
+   Client.send_message(chat_id,"عضو زليل")
   elif rank == "restricted":
-   app.send_message(chat_id,"عضو متقيد")
+   Client.send_message(chat_id,"عضو متقيد")
   elif rank == "left":
-   app.send_message(chat_id,"انتا مغادر يعم")
+   Client.send_message(chat_id,"انتا مغادر يعم")
   elif rank == "kicked":
-   app.send_message(chat_id,"الراجل ده واخد بالجزمه ومحظور")
+   Client.send_message(chat_id,"الراجل ده واخد بالجزمه ومحظور")
 
 
 @Client.on_message(command(["ping", "ينج", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
