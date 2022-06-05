fa3850d98f5afb1013d320feb912a64188cf5b55
umgihtgkr <105083015+umgihtgkr@users.noreply.github.com>
2022-06-05 14:36:44 +0200

    Update new.py

--- program/new.py
+++ program/new.py
@@ -10,14 +10,14 @@ def forward(client, message):
   rank =  await.get_chat_member(chat_id, user_id)
   rank = rank.status
   if rank == "administrator":
-    await.send_message(chat_id,"مسؤال")
+    Client.send_message(chat_id,"مسؤال")
   elif rank == "creator":
-    await.send_message(chat_id,"المنشئ يعم")
+    Client.send_message(chat_id,"المنشئ يعم")
   elif rank == "member":
-    await.send_message(chat_id,"عضو زليل")
+    Client.send_message(chat_id,"عضو زليل")
   elif rank == "restricted":
-    await.send_message(chat_id,"عضو متقيد")
+    Client.send_message(chat_id,"عضو متقيد")
   elif rank == "left":
-    await.send_message(chat_id,"انتا مغادر يعم")
+    Client.send_message(chat_id,"انتا مغادر يعم")
   elif rank == "kicked":
     await.send_message(chat_id,"الراجل ده واخد بالجزمه ومحظور")
