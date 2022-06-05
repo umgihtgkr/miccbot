006dd4f278dc5cfbe532112e8164a9083905550e
umgihtgkr <105083015+umgihtgkr@users.noreply.github.com>
2022-06-05 06:24:11 +0200

    Update start.py

--- program/start.py
+++ program/start.py
@@ -1,4 +1,7 @@
 import asyncio
+import pyrogram
+import time
+import os
 
 from datetime import datetime
 from sys import version_info
@@ -177,6 +180,26 @@ async def start(client: Client, message: Message):
          ]
      )
   )
+
+
+@Client.on_message(filters.command("رتبتي"))
+def forward(client, message):
+  chat_id = message.chat.id
+  user_id = message.from_user.id
+  rank = app.get_chat_member(chat_id, user_id)
+  rank = rank.status
+  if rank == "administrator":
+   app.send_message(chat_id,"مسؤال")
+  elif rank == "creator":
+   app.send_message(chat_id,"المنشئ يعم")
+  elif rank == "member":
+   app.send_message(chat_id,"عضو زليل")
+  elif rank == "restricted":
+   app.send_message(chat_id,"عضو متقيد")
+  elif rank == "left":
+   app.send_message(chat_id,"انتا مغادر يعم")
+  elif rank == "kicked":
+   app.send_message(chat_id,"الراجل ده واخد بالجزمه ومحظور")
 
 
 @Client.on_message(command(["ping", "ينج", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
